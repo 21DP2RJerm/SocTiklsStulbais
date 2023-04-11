d490af550cefa432605de89dbfc34527a7fbbd80
21DP2RJerm <100911489+21DP2RJerm@users.noreply.github.com>
2023-04-11 23:08:12 +0300

    Add files via upload

--- src/views/PersonalProfile.vue
+++ src/views/PersonalProfile.vue
@@ -1,7 +1,29 @@
 <template>
-  <h1>User info</h1>
-  <div v-for="user in userInfo" :key="user.id">
-    <h2>Username: {{ user.username }}<br>Name: {{ user.name }}</h2>
+  <div class="parent">
+    <div v-for="user in userInfo" :key="user.id">
+      <a id="username" class="font-head">{{ user.username }}</a>
+      <div id="profileabout">
+        <a id="ProfileImage" style="float: left; "><img src="src/icons/FtXgiG7WAAAaIrT.png" alt="kristaps" style="width:300px;height:400px; outline: 3px solid white; margin: 10px;"></a>
+        <a id="textHeightChange" class="font-head"  style="float: left; " >{{ user.name }}</a><br>
+        <a class="font-regular" style="float: left; width: 680px; ">{{ user.bio }}</a>
+      </div>
+    </div>
+    <br>
+    <div id="profileposts">
+      <div class="split one">
+        
+      </div>
+      <div class="split two">
+
+      </div>
+      <div class="split three">
+
+      </div>
+      <div class="split four">
+
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -10,13 +32,49 @@ export default {
     data(){
         return{
             userInfo: [
-                { username: 'Kristzz', name: 'Krists', id: 1},
-            ]
+                { username: 'Kristzz', name: 'Krists', bio: 'Armīnieks, 18 gadu vecumā ar 20 gadu stāžu dienestā, garšo mellenes, māk apieties ar datoru. Izgudroju Hyperlink un Steelseries Intel Core Threadripper 200k ', id: 1},
+            ] 
         }
     }
 }
 </script>
 
 <style>
-
+  #username{
+    width: 1000px;
+    height: 30px;
+    background-color: var(--color-container);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #profileabout{
+    width: 1000px;
+    min-height: 400px;
+    background-color: #f6e3ba;
+    border-radius:30px;
+    float: left;
+    text-align: justify;
+  }
+  #profileposts{
+    width: 1000px;
+    min-height: 1000px;
+    background-color: #ffefcb;
+    
+  }
+  #textHeightChange{
+    
+    font-size: 20px;
+  }
+  
+  .split{
+    margin: 5px;
+    width: 240px;
+    height: 320px;
+    background-color: white;
+    float: left;
+    
+    
+  }
+  
 </style>
